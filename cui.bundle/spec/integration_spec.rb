a85cec8bf88e69d68db7492a2440c12389bc7c10
Taku Nakajima <tnaka@dc4.so-net.ne.jp>
2013-08-03 18:04:48 +0900

    integration test for search and play with queue

--- cui.bundle/spec/integration_spec.rb
+++ cui.bundle/spec/integration_spec.rb
@@ -73,6 +73,56 @@ describe "SevenMinutes integration test" do
   end
 
   it "should play track with DummyPlayer" do
+    setup_dummy_player
+    click_link '7m_demo'
+    sleep 1.0
+    page.body.must_match /Deux Arabesques/
+    click_link 'Play!'
+    sleep 1.0
+    page.body.must_match /Deux Arabesques/
+    find(:css, 'div[data-role="header"] h1').text.must_equal 'Playing'
+    page.evaluate_script('App.playing.get("status")').must_equal 3 # App.Status.PLAYING
+  end
+
+  it "should search and play with queue" do
+    setup_dummy_player
+
+    # search tracks
+    click_link 'My Library'
+    within("#search-ul") do
+      click_link 'search'
+    end
+    sleep 1.0
+    find('#search-word').set('Deux')
+    page.execute_script "$('#search-word').trigger('keyup')"
+    sleep 3.0
+    page.body.must_match /Deux Arabesques/
+
+    # add it to queue
+    click_link 'Deux Arabesques: No 1. Andantino con moto'
+    sleep 1.0
+    click_link 'Add to Queue'
+    sleep 3.0
+    page.evaluate_script('App.playing.get("status")').must_equal 3 # App.Status.PLAYING
+
+    # add another track to queue
+    click_link 'Deux Arabesques: No 2. Allegretto scherzando'
+    sleep 1.0
+    click_link 'Add to Queue'
+
+    # reload and check Queue
+    visit '/'
+    sleep 1.0
+    click_link 'My Library'
+    within("#search-ul") do
+      click_link 'queue'
+    end
+    sleep 1.0
+    page.body.must_match /Deux Arabesques: No 1. Andantino con moto/
+    page.body.must_match /Deux Arabesques: No 2. Allegretto scherzando/
+  end
+
+  def setup_dummy_player
     visit '/#config'
     sleep 1.0
     page.body.must_match /SevenMinutes/
@@ -85,14 +135,7 @@ describe "SevenMinutes integration test" do
 
     click_link 'config-save'
     page.body.must_match /7mtest/
-    click_link '7m_demo'
-    sleep 1.0
-    page.body.must_match /Deux Arabesques/
-    click_link 'Play!'
-    sleep 1.0
-    page.body.must_match /Deux Arabesques/
-    find(:css, 'div[data-role="header"] h1').text.must_equal 'Playing'
-    page.evaluate_script('App.playing.get("status")').must_equal 3 # App.Status.PLAYING
+
   end
 end
  
